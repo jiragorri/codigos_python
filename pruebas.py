7a7ee4751d772b458d26ecbaf01dfb0f04c6619b
Joan Iragorri <joan.iragorri@gmail.com>
2023-09-28 10:33:06 -0500

    prueba de cambio

--- pruebas.py
+++ pruebas.py
@@ -1,17 +1,4 @@
-'''cont = 1
-cont_prim = 0
-num = int(input("ingrese un numero: "))
 
-while num >= cont:
-    if num % cont == 0:
-        cont_prim += 1
-    print(cont_prim)
-    cont += 1
-
-if cont_prim > 2:
-    print("el numero",num,"no es primo")
-else:
-    print("el numero",num,"numero primo")'''
 
 print("Imprimir los primeros 10 números primos usando un bucle while")
 cant_prim = 0
